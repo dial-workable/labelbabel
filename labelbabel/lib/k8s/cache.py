cd0a6eb1e0316932bf1ed4cf605d4bab48967ca0
dial-workable <aloupis@workable.com>
2021-03-22 14:18:08 +0200

    ADD EventCache implemntation and tests

--- labelbabel/lib/k8s/cache.py
+++ labelbabel/lib/k8s/cache.py
@@ -1,16 +1,69 @@
 # -*- coding: utf-8 -*-
 
 import labelbabel.lib.k8s as k8s
+import hashlib
 
-class EventCache():
+
+class EventCache:
+    """EventCache is a class which is meant to act as a deduplication cache for PodEvent objects.
+    It is primarily meant to be used by the labelbabel.lib.k8s.Scraper class to filter-out the
+    pod modification events which are not modifying any fields we are interested in.
+    """
+
     def __init__(self):
-        self._cache = {}
+        self._cache = set()
 
-    def add_event(self):
-        return True
+    def _calculate_hash(self, event):
+        """_calculate_hash is a function which calculates a hash from a given labelbabel.lib.k8s.PodEvent namedtuple.
 
-    def message(slef):
-        return k8s.PodEvent('type', 'cluster', 'ns', 'name', 'labels', 'start_time', 'uid')
+        Args:
+            event (labelbabel.lib.k8s.PodEvent): The event object for which we want to calculate the hash
 
-    def get_checksum(slef, event: k8s.PodEvent):
-        pass
+        Returns:
+            str: The calculated has is based on the uid and the labels of the given object. Ordering of the labels
+                in the event do not matter.
+        """
+        hasher = hashlib.md5()
+
+        hasher.update(event.uid.encode("utf-8"))
+
+        sorted_keys = [] if not event.labels else sorted(event.labels.keys())
+        for k in sorted_keys:
+            hasher.update(k.encode("utf-8"))
+            hasher.update(event.labels[k].encode("utf-8"))
+
+        return hasher.hexdigest()
+
+    def add_event(self, event):
+        """add_event method is used to add a new event into the cache.
+        The method calculates a hash form the data in the event to check if
+        a similar object exists inside the cache.
+
+        Args:
+            event (labelbabel.lib.k8s.PodEvent): This is the event we are trying to add in the cache.
+
+        Returns:
+            bool: True if the added event has been successfully added.
+                False if the object was already added in the cache.
+        """
+        if not isinstance(event, k8s.PodEvent):
+            raise TypeError(
+                "add_event method only accepts parameters of type labelbabel.lib.k8s.PodEvent"
+            )
+
+        hash = self._calculate_hash(event)
+        if hash not in self._cache:
+            self._cache.add(hash)
+            return True
+
+        return False
+
+    def clear(self):
+        """clear method can be used to clear the cache from all the previously stored items."""
+        self._cache = set()
+
+    def __contains__(self, item):
+        """__contains__ magic method implementation allows us to use the "in" operator on the EventCache object"""
+        return isinstance(item, k8s.PodEvent) and (
+            self._calculate_hash(item) in self._cache
+        )
--- tests/unit/lib/k8s/test_cache.py
+++ tests/unit/lib/k8s/test_cache.py
@@ -2,14 +2,142 @@ from labelbabel.lib.k8s import cache
 
 import labelbabel.lib.k8s as k8s
 import unittest
+import datetime
+
+TEST_EVENT_DATA = [
+    {
+        "event": k8s.PodEvent(
+            "cluster",
+            "ns",
+            "name",
+            {"app": "application1", "process": "web"},
+            datetime.datetime(2021, 2, 12, 18, 11, 5),
+            datetime.datetime(2021, 2, 13, 19, 15, 25),
+            "dsasdf-adsf-asdf-faf2323f4rdsfs-3432-f3fefs-3243fwefw",
+        ),
+        "expected": "c83cdafef94251b535616814c57da72e",
+    },
+    {
+        "event": k8s.PodEvent(
+            "cluster",
+            "ns",
+            "name",
+            {"process": "web", "app": "application1"},
+            datetime.datetime(2021, 2, 12, 18, 11, 5),
+            datetime.datetime(2021, 2, 13, 19, 15, 25),
+            "dsasdf-adsf-asdf-faf2323f4rdsfs-3432-f3fefs-3243fwefw",
+        ),
+        "expected": "c83cdafef94251b535616814c57da72e",
+    },
+    {
+        "event": k8s.PodEvent(
+            "cluster",
+            "ns",
+            "name",
+            {},
+            datetime.datetime(2021, 2, 12, 18, 11, 5),
+            datetime.datetime(2021, 2, 13, 19, 15, 25),
+            "dsasdf-adsf-asdf-faf2323f4rdsfs-3432-f3fefs-3243fwefw",
+        ),
+        "expected": "fb78ccff9715954b486f6a2634209b2b",
+    },
+    {
+        "event": k8s.PodEvent(
+            "cluster",
+            "ns",
+            "name",
+            None,
+            datetime.datetime(2021, 2, 12, 18, 11, 5),
+            None,
+            "dsasdf-adsf-asdf-faf2323f4rdsfs-3432-f3fefs-3243fwefw",
+        ),
+        "expected": "fb78ccff9715954b486f6a2634209b2b",
+    },
+]
+
+
+TEST_EVENT_DATA_UNIQUE = [
+    {
+        "event": k8s.PodEvent(
+            "cluster",
+            "ns",
+            "name",
+            {"process": "web", "app": "application1"},
+            datetime.datetime(2021, 2, 12, 18, 11, 5),
+            datetime.datetime(2021, 2, 13, 19, 15, 25),
+            "dsasdf-adsf-asdf-faf2323f4rdsfs-3432-f3fefs-3243fwefw",
+        ),
+        "expected": "c83cdafef94251b535616814c57da72e",
+    },
+    {
+        "event": k8s.PodEvent(
+            "cluster",
+            "ns",
+            "name",
+            {},
+            datetime.datetime(2021, 2, 12, 18, 11, 5),
+            datetime.datetime(2021, 2, 13, 19, 15, 25),
+            "dsasdf-adsf-asdf-faf2323f4rdsfs-3432-f3fefs-3243fwefw",
+        ),
+        "expected": "fb78ccff9715954b486f6a2634209b2b",
+    },
+]
 
 
 class CacheTestCase(unittest.TestCase):
-    def test_message(self):
+    def test_podevent_hash(self):
+        """test the hashing algorithm for the PodEvent objects"""
         c = cache.EventCache()
-        self.assertEqual(
-            c.message(),
-            k8s.PodEvent(
-                "type", "cluster", "ns", "name", "labels", "start_time", "uid"
-            ),
-        )
+        count = 0
+        for data in TEST_EVENT_DATA:
+            self.assertEqual(
+                c._calculate_hash(data["event"]),
+                data["expected"],
+                f"Testing dataset with index(zero-based) {count}",
+            )
+            count += 1
+
+    def test_chach_clearing(self):
+        """test the cache clearing method
+        The test load and clears the cache two time to catch possible regression errors in the cases
+        where we add add items in a previously "cleared" cache.
+        """
+        c = cache.EventCache()
+
+        # Load test events
+        count = 0
+        for data in TEST_EVENT_DATA_UNIQUE:
+            c.add_event(data["event"])
+            self.assertTrue(
+                data["event"] in c,
+                f"Item with index(zero-based) {count} should exist in the cache",
+            )
+            count += 1
+
+        # Remove all test events
+        c.clear()
+
+        # Load test events
+        count = 0
+        for data in TEST_EVENT_DATA_UNIQUE:
+            self.assertFalse(
+                data["event"] in c,
+                f"Item with index(zero-based) {count} should not exist in the cache",
+            )
+            self.assertTrue(
+                c.add_event(data["event"]),
+                f"Item with index(zero-based) {count} should be missing from the cache",
+            )
+            count += 1
+
+        # Remove all test events
+        c.clear()
+
+        # Load test events
+        count = 0
+        for data in TEST_EVENT_DATA_UNIQUE:
+            self.assertTrue(
+                c.add_event(data["event"]),
+                f"Item with index(zero-based) {count} should be missing from the cache after 2nd clearing",
+            )
+            count += 1
